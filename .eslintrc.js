c036d66b4a65dac3dde895567bbc96019aa4b9e8
Vertigo <vertigomaill@gmail.com>
2021-11-15 23:19:30 +0300

    feat: install dependencies, config eslint\husky

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base/legacy',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': 'off',
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      env: {
+        es6: true,
+        node: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        indent: ['error', 2],
+        'linebreak-style': 'off',
+        quotes: ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+        semi: ['error', 'always'],
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
